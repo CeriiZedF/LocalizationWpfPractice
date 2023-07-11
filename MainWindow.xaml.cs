ed52b1e8c2f2fd30b191164ea5718d2b114c9c9a
CeriiZedF <dasbalan14@gmail.com>
2023-07-11 17:13:05 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalizationWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = sender as Button;
+           
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(temp.Tag.ToString());
+            b1.Content = Localization.b1;
+            b2.Content = Localization.b2;
+            b3.Content = Localization.b3;
+        }
+    }
+}
